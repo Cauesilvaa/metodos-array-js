a42e56610d302c0fcc82d3f00f53d539f3d0df33
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2022-08-15 17:14:58 -0300

    Update sort e reverse.js

--- sort e reverse/sort e reverse.js
+++ sort e reverse/sort e reverse.js
@@ -10,3 +10,20 @@ console.log(frutas);
 // REVERSE
 frutas.reverse();
 console.log(frutas);
+
+
+/*********************************************************************************************************** */
+const items = [
+    { name: 'Edward', value: 21 },
+    { name: 'Sharpe', value: 37 },
+    { name: 'And', value: 45 },
+    { name: 'The', value: -12 },
+    { name: 'Zeros', value: 37 }
+  ];
+  
+  
+  const a = items.sort((value, value2)=> {
+    return value.value - value2.value
+  });
+  
+  console.log(a.reverse());
